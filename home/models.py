1de66949a115b655af334ec8ad996aacc2888c43
Chris Rogers <chrxr@hotmail.co.uk>
2016-07-29 09:49:49 +0100

    more model pruning

--- home/models.py
+++ home/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 
-from wagtail.wagtailadmin.edit_handlers import (FieldPanel)
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
 class HomePage(Page):
     body = RichTextField(null=True, blank=True)
